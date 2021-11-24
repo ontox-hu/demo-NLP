acd1234fe924db16c6b64c259fffa8983c6ec7e0
MarieCo <corradi.marie@gmail.com>
2021-11-24 13:26:45 +0100

    changed default text, removed some default settings

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import spacy_streamlit
 
 models = ["en_tox"]
-default_text = "Concentrations of diethylstilboestrol (DES) exceeding 100 microM are normally fatal to all living tissues."
-spacy_streamlit.visualize(models, default_text)
+title = "NLP TOX"
+description = "Text analysis with NLP model trained on toxicological articles"
+default_text = "Benzene in concentrations higher than 3mg can cause vomiting in humans."
+spacy_streamlit.visualize(models,default_text, visualizers = [ "ner", "parser", "similarity"], show_json_doc = False,show_meta = False, show_config = False, sidebar_title = title, sidebar_description = description)
