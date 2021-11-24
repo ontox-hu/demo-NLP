e16ac8bc4d5f21ba1f55cf9c9fbe36d6d404e7e7
MarieCo <corradi.marie@gmail.com>
2021-11-24 13:46:25 +0100

    changed tokens and similarity defaults

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,8 @@ models = ["en_tox"]
 title = "NLP TOX"
 description = "Text analysis with NLP model trained on toxicological articles"
 default_text = "Benzene in concentrations higher than 3mg can cause vomiting in humans."
-spacy_streamlit.visualize(models,default_text, visualizers = [ "ner", "parser", "similarity"], \
-                          similarity_texts = ("ethanol", "methanol"),
+token_attributes = ["idx", "text", "lemma_", "pos_", "tag_", "dep_","ent_type_"]
+spacy_streamlit.visualize(models,default_text, visualizers = [ "ner", "parser", "similarity", "tokens"],\
+                          similarity_texts = ("methanol", "ethanol"), token_attrs = token_attributes,\
                           show_json_doc = False,show_meta = False, show_config = False, \
                           sidebar_title = title, sidebar_description = description)
