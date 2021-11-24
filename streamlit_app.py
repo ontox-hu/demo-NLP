0cd47aebb5ca98c52f4543c7e8e030aa3d53623d
MarieCo <corradi.marie@gmail.com>
2021-11-24 13:39:13 +0100

    some more defaults changed (tokens, similarity text)

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,7 @@ models = ["en_tox"]
 title = "NLP TOX"
 description = "Text analysis with NLP model trained on toxicological articles"
 default_text = "Benzene in concentrations higher than 3mg can cause vomiting in humans."
-spacy_streamlit.visualize(models,default_text, visualizers = [ "ner", "parser", "similarity"], show_json_doc = False,show_meta = False, show_config = False, sidebar_title = title, sidebar_description = description)
+spacy_streamlit.visualize(models,default_text, visualizers = [ "ner", "parser", "similarity"], \
+                          similarity_texts = ("ethanol", "methanol"),
+                          show_json_doc = False,show_meta = False, show_config = False, \
+                          sidebar_title = title, sidebar_description = description)
