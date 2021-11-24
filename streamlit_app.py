dfc45f14558a11119e25018d55c72cc601d7e4a7
MarieCo <corradi.marie@gmail.com>
2021-11-24 14:04:39 +0100

    select visualizers

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,15 @@
 import spacy_streamlit
+import streamlit as st
 
 models = ["en_tox"]
 title = "NLP TOX"
 description = "Text analysis with NLP model trained on toxicological articles"
 default_text = "Benzene in concentrations higher than 3mg can cause vomiting in humans."
-token_attributes = ["idx", "text", "lemma_", "pos_", "tag_", "dep_","ent_type_"]
+token_attributes = ["idx", "text", "pos_", "tag_", "dep_","ent_type_"]
 spacy_streamlit.visualize(models,default_text, visualizers = [ "ner", "parser", "similarity", "tokens"],\
                           similarity_texts = ("methanol", "ethanol"), token_attrs = token_attributes,\
                           show_json_doc = False,show_meta = False, show_config = False, \
+                          show_visualizer_select = True,\
                           sidebar_title = title, sidebar_description = description)
+
+
